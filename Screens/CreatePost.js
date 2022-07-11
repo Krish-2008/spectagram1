f1522538b587f718a1b198f2ce33df6b1616aaa8
Krish-2008 <89650110+Krish-2008@users.noreply.github.com>
2022-07-11 15:30:39 +0530

    Add files via upload

---
+++ Screens/CreatePost.js
@@ -0,0 +1,21 @@
+import React from 'React';
+import {View,StyleSheet,Text} from 'react.js';
+
+
+export default function CreatePost(){
+    <View>
+        <Text style={styles.text}>CreatePost</Text>
+    </View>
+}
+
+
+const styles = StyleSheet.create({
+    Text:{
+        fontSize:30,
+        fontWeight:"bold",
+        color:"black"
+
+
+    }
+
+})
